6abd31d54d11f632b0de8cb0bb425e6c6b541b19
Artem Sviridenko <svirid@rarus.ru>
2021-05-09 23:34:44 +0300

    Stage I

---
+++ src/main/java/com/skillbox/blogapp/security/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Security configuration.
+ */
+package com.skillbox.blogapp.security;
---
+++ src/main/java/com/skillbox/blogapp/web/rest/ApiPostController.java
@@ -0,0 +1,8 @@
+package com.skillbox.blogapp.web.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/post/")
+public class ApiPostController {
+
+}
